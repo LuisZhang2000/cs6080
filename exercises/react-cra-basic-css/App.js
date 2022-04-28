aefca74bdfc3c906b169edb1285f394e636196e4
Luis Zhang <42627737+LuisZhang2000@users.noreply.github.com>
2022-04-28 14:14:22 +1000

    Add files via upload

---
+++ exercises/react-cra-basic-css/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>You are safe now</h1>
+      <p className="small-text">human</p>
+      <img alt="" src="https://i.ytimg.com/vi/wWqdhBdeMSg/hqdefault.jpg"/>
+      <p className="body-text">You either die a hero, or live long enough to see yourself become the villain:</p>
+      <ul>
+        <li><a href="https://i.ytimg.com/vi/wWqdhBdeMSg/hqdefault.jpg">Cat Video 1</a></li>
+        <li><a href="https://i.ytimg.com/vi/wWqdhBdeMSg/hqdefault.jpg">Cat Video 2</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
