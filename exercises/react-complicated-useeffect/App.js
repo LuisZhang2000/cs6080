36146f74c7c0cc98c65306c9a98fb0841da7c728
Luis Zhang <42627737+LuisZhang2000@users.noreply.github.com>
2022-05-01 11:57:18 +1000

    Add files via upload

---
+++ exercises/react-complicated-useeffect/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const [aRerenders, setARerenders] = React.useState(0);
+  const [bRerenders, setBRerenders] = React.useState(0);
+
+  const [effectRerenders, setEffectRerenders] = React.useState(0);
+
+  React.useEffect(() => {
+    setEffectRerenders(effectRerenders + 1)
+  }, [aRerenders, bRerenders]);
+
+  return (
+    <div className="App">
+      <div className="vertMargins">
+        'A' re-render count: {aRerenders}
+        <button onClick={() => setARerenders(aRerenders + 1)}>
+          +
+        </button>
+      </div>
+      <div className="vertMargins">
+        'B' re-render count: {bRerenders}
+        <button onClick={() => setBRerenders(bRerenders + 1)}>
+          +
+        </button>
+      </div>
+      <div>
+        {effectRerenders}
+      </div>
+    </div>
+  );
+}
+
+export default App;
